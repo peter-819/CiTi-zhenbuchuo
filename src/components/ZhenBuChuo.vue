68926167c4972988008359c07b93e869a67a362b
peter-819 <peter-819@outlook.com>
2020-10-07 13:31:51 +0800

    Just play

---
+++ src/components/ZhenBuChuo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class = "ZhenBuChuo">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZhenBuChuo',
+  data () {
+    return {
+      msg: '真不戳'
+    }
+  }
+}
+</script>
