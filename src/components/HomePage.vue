b1a746d9be3ef602a4c5a6195a7462d78a038335
fzegg <2018302110301@whu.ed.cn>
2020-10-10 21:24:24 +0800

    add introContext

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="HomePage">
-    <heading></heading>
-    <div id="block"></div>
-    <div id="introContexts" style="width: 1920px;height:1000px ">
-     <intro-context id="context1"></intro-context>
+    <div><heading></heading></div>
+    <div class="block"></div>
+    <div class="introContexts">
+      <intro-context class="context1"></intro-context>
+      <intro-context class="context2"></intro-context>
+      <intro-context class="context2"></intro-context>
     </div>
   </div>
 
@@ -12,25 +14,34 @@
 <script>
   import heading from "./heading";
   import introContext from "./introContext";
-    export default {
-      name: "HomePage",
-      components:{
-        heading,
-        introContext
-      }
+  export default {
+    name: "HomePage",
+    components:{
+      heading,
+      introContext
     }
+  }
 </script>
 
-<style scoped>
- #block{
+<style>
+ .block{
    width: 1920px;
    height: 927px;
    background: #29282D;
    opacity: 1;
  }
-  #context1{
-    position: relative;
-    top:222px;
-    left:96px;
+ .introContexts{
+   display: flex;
+   -webkit-flex-direction: row;
+   width: 1823px;
+   margin-left: 96px;
+   margin-top: 222px;
+ }
+ .context1{
+   margin-left:0px;
   }
+ .context2{
+   margin-left: 100px;
+ }
+
 </style>
