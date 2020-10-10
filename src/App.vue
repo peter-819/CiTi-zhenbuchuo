e46add0a39a7f03a208c3984357b969a245e519c
fzegg <2018302110301@whu.ed.cn>
2020-10-10 22:00:56 +0800

    add elementui

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
 
 <script>
 import '@vant/touch-emulator';
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import Vue from 'vue';
+Vue.use(ElementUI);
 export default {
   name: 'App'
 }
