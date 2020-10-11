6cba29950f1b141402f7bf17e39244d6dc873c78
peter <172163600@qq.com>
2020-10-11 21:14:36 +0800

    add intro-context

--- src/components/introContext.vue
+++ src/components/introContext.vue
@@ -1,10 +1,30 @@
 <template>
-    <div id="introContext"></div>
+    <div id="introContext">
+      <img :src="Contents.ImageSrc"></img>
+      <h1 class="ICTitleFont">{{Contents.Title}}</h1>
+      <p class="ICContentFont" v-html="Contents.Content"></p>
+    </div>
 </template>
 
 <script>
     export default {
       name: "introContext",
+      props:{
+        Contents:{
+          Title:{
+            type: String,
+            require: true
+          },
+          Content:{
+            type: String,
+            require: true
+          },
+          ImageSrc:{
+            type: String,
+            require: true
+          }
+        }
+      },
       data(){
         return{
 
@@ -21,5 +41,24 @@
     box-shadow: 0px 3px 6px rgba(112, 112, 112, 0.16);
     opacity: 1;
     border-radius: 15px;
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+  }
+  .ICTitleFont{
+    font-size: 34px;
+    font-family: Soucrce Han Sans CN;
+    font-weight: 400;
+    line-height: 58px;
+    color: #2CB8B9;
+    opacity: 1;
+  }
+  .ICContentFont{
+    font-size: 22px;
+    font-family: Source Han Sans CN;
+    font-weight: 400;
+    line-height: 37px;
+    color: #707070;
+    opacity: 0.65;
   }
 </style>
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -2,10 +2,18 @@
   <div id="HomePage">
     <div><heading></heading></div>
     <div class="block"></div>
-    <div class="introContexts">
-      <intro-context class="context1"></intro-context>
-      <intro-context class="context2"></intro-context>
-      <intro-context class="context2"></intro-context>
+      <div>
+        <h1 class="TitleFont">系统特点</h1>
+        <div style="display:flex; justify-content:center;">
+        <img src="/static/HomePage/Polygon1.png" style="width:29px;height:14px;color:#F5F6FA;"></img>
+      </div>
+      <div class="introContexts">
+        <intro-context class="context" :contents="context1"></intro-context>
+        <intro-context class="context" :contents="context1"></intro-context>
+        <intro-context class="context" :contents="context1"></intro-context>
+        <!-- <intro-context class="context2"></intro-context>
+        <intro-context class="context2"></intro-context> -->
+      </div>
     </div>
     <div><bottom></bottom></div>
   </div>
@@ -18,6 +26,15 @@
   import bottom from "../components/bottom.vue";
   export default {
     name: "HomePage",
+    data(){
+      return{
+        context1:{
+          Title:'技术亮点',
+          Content:'这里放具体的内容内容内容 <br> 这里放具体的内容内容内容 <br> 这里放具体的内容内容内容 <br> 这里放具体的内容内容内容 <br> 这里放具体的内容内容内容<br>',
+          ImageSrc:'/static/HomePage/Bulb.png'
+        }
+      }
+    },
     components:{
       heading,
       introContext,
@@ -37,14 +54,22 @@
    display: flex;
    -webkit-flex-direction: row;
    width: 100%;
-   margin-left: 96px;
-   margin-top: 222px;
+   height: 1000px;
+   margin-left:49px;
+   margin-right:49px;
  }
- .context1{
-   margin-left:0px;
+ .context{
+   margin:98px 49px 98px 49px;
   }
- .context2{
-   margin-left: 100px;
- }
+  .TitleFont{
+    font-size: 44px;
+    font-family: Source Han Sans CN;
+    font-weight: 400;
+    line-height: 75px;
+    color: #29282D;
+    letter-spacing: 40;
+    opacity: 1;
+    text-align: center;
+  }
 
 </style>
