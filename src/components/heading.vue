bb329deb0591c3c2ae44eef90556ad478f1d14fb
fzegg <2018302110301@whu.ed.cn>
2020-10-11 17:11:16 +0800

    add heading

--- src/components/heading.vue
+++ src/components/heading.vue
@@ -1,31 +1,47 @@
 <template>
-<div id="rectangle">
-  <div id="logo">名字logo</div>
-</div>
-
+  <div class="rectangle">
+    <el-row>
+      <el-col v-for="(item,index) in cols" :span="item.span" :offset="item.offset" :key="index">
+        <div class="HeaderComponent" :style="item.sty">
+          {{item.context}}
+          <img v-if = "item.src!=null" :src="item.src">
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-  export default{
-    name:"heading",
-    data(){
-      return{
+  export default {
+    data() {
+      return {
+        cols:[
+          {span:"2", context:"名字", offset:"1", sty:"font-size: 20px; opacity:0.9;"},
+          {span:"2", context:"首页", offset:"9", sty:"font-weight:500; color: #2CB8B9;"},
+          {span:"3", context:"借贷业务"},
+          {span:"2", context:"新闻"},
+          {span:"2", src:"./static/HomePage/message.png",offset:"2", sty:"margin-top:8px;"},
+          {span:"1", src:"./static/HomePage/admin.png", sty:"margin-top:8px;"},
 
+        ]
       };
     }
   }
 
 </script>
 
-<style scoped>
-  #rectangle{
-    width: 1920px;
+<style>
+  .rectangle{
+    width: 100%;
     height: 74px;
-    background: #707070;
-    opacity: 0.45;
+    background: rgba(112,112,112,0.45);
     position: absolute;
   }
-  #logo{
-
+  .HeaderComponent{
+    font-size: 26px;
+    font-family: Source Han Sans CN;
+    font-weight: 400;
+    line-height: 74px;
+    color: #FFFFFF;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage'
+import HomePage from '@/page/HomePage'
 
 Vue.use(Router)
 
