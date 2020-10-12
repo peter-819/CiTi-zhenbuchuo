b9b80d0c579498e70076659629d8d22ffedfae02
peter <172163600@qq.com>
2020-10-12 11:23:44 +0800

    center display intro context

--- src/components/introContext.vue
+++ src/components/introContext.vue
@@ -35,8 +35,6 @@
 
 <style scoped>
   #introContext{
-    width: 509px;
-    height: 540px;
     background: #FFFFFF;
     box-shadow: 0px 3px 6px rgba(112, 112, 112, 0.16);
     opacity: 1;
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="HomePage">
     <div><heading></heading></div>
-    <div class="block">
-      <div class="img1container"><img class="img1" src="/static/HomePage/hleft.png"></img></div>
-      <div class="img2container"><img class="img2" src="/static/HomePage/hrightup.png"></img></div>
-    </div>
+    <div class="block"></div>
       <div>
         <h1 class="TitleFont">系统特点</h1>
         <div style="display:flex; justify-content:center;">
         <img src="/static/HomePage/Polygon1.png" style="width:29px;height:14px;color:#F5F6FA;"></img>
       </div>
-      <div class="introContexts">
-        <intro-context class="context" :contents="context1"></intro-context>
-        <intro-context class="context" :contents="context1"></intro-context>
-        <intro-context class="context" :contents="context1"></intro-context>
-        <!-- <intro-context class="context2"></intro-context>
-        <intro-context class="context2"></intro-context> -->
-      </div>
+      <el-row class="introContexts">
+        <el-col :span="8" style="display: flex;align-items: center;justify-content: center;"><intro-context class="context" :contents="context1"></intro-context></el-col>
+        <el-col :span="8" style="display: flex;align-items: center;justify-content: center;"><intro-context class="context" :contents="context1"></intro-context></el-col>
+        <el-col :span="8" style="display: flex;align-items: center;justify-content: center;"><intro-context class="context" :contents="context1"></intro-context></el-col>
+      </el-row>
     </div>
     <div><bottom></bottom></div>
   </div>
@@ -49,41 +44,18 @@
 <style>
  .block{
    width: 100%;
-   height: 800px;
+   height: 927px;
    background: #29282D;
    opacity: 1;
  }
- .img1container{
-   margin-left: 20px;
-   width:41%;
-   height:100%;
-   display: flex;
-   flex-direction: row;
-   align-items: center;
-
- }
- .img1{
-      max-width:100%;
-      height:auto;
- }
- .img2container{
-   margin-top: -74px;
-   width:39%;
-   height:40%;
-   display: flex;
-   flex-direction: row;
-   align-items: center;
- }
  .introContexts{
-   display: flex;
-   -webkit-flex-direction: row;
    width: 100%;
    height: 1000px;
-   margin-left:49px;
-   margin-right:49px;
+   margin-top:100px;
  }
  .context{
-   margin:98px 49px 98px 49px;
+   width: 509px;
+   height:540px;
   }
   .TitleFont{
     font-size: 44px;
