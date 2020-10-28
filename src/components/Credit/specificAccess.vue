d39ed32664473df67543d4dfe34e812a08cdd812
fzegg <2018302110301@whu.ed.cn>
2020-10-28 15:37:29 +0800

    edit upload in specificAccess.vue

--- src/components/Credit/specificAccess.vue
+++ src/components/Credit/specificAccess.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="specificAccess">
+  <div id="specificAccess" style="overflow: auto">
     <div class="block">
       <div style="display: flex;flex-direction: row;align-items:center;margin-left:615px;margin-top: 11px">
         <img src="/static/CreditHomePage/pic152.png">
@@ -10,20 +10,22 @@
       </div>
       <el-upload
         style="margin-left:103px;margin-top: 23px;width: 364px"
-        drag
-        action="https://jsonplaceholder.typicode.com/posts/"
-        multiple
         ref="upload"
         multiple
-        :on-success="handleSuccess"
+        action="#"
+        accept=".jpg,.pdf.,.png"
+        :on-change="handleChange"
         :before-upload="beforeUpload"
+        :before-remove="beforeRemove"
         :on-preview="handlePreview"
         :on-remove="handleRemove"
         :file-list="fileList"
         :auto-upload="false">
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png/pdf文件，且不超过10MB</div>
+        <div style="display: flex;flex-direction: column;align-items: center">
+          <img src="/static/CreditHomePage/submit.png">
+          <div class="el-upload__tip" slot="tip">只能上传jpg/png/pdf文件，且不超过10MB</div>
+        </div>
+
       </el-upload>
       <div style="width: 224px;height: 30px;font-size: 30px;font-family: Source Han Sans CN;font-weight: 400;line-height: 30px;color: #707070;margin-left: 103px;margin-top: 40px;">
         添加声明：
@@ -38,7 +40,7 @@
         maxlength="500"
         show-word-limit>
       </el-input>
-      <el-button style="background-color: #2CB8B9;color: #FFFFFF;font-size:22px;width: 130px;height: 46px;border-radius: 10px;margin-left: 658px;margin-top: 98px" type="success" @click="submitUpload">提交</el-button>
+      <el-button style="background-color: #2CB8B9;color: #FFFFFF;font-size:22px;width: 130px;height: 46px;border-radius: 10px;margin-left: 658px;margin-top: 98px;margin-bottom: 90px" type="success" @click="submitUpload">提交</el-button>
     </div>
   </div>
 </template>
@@ -51,33 +53,90 @@
           fileList:[]
         }
       },
-      methods:{
+      methods: {
+        uploadFile(item, policyData) {
+          console.log(item);
+          var formData = new FormData();
+          formData.append("OSSAccessKeyid", policyData.accessid);
+          formData.append("key", policyData.dir + Date.parse(new Date()) + this.getSuffix(item));
+          formData.append("policy", policyData.policy);
+          formData.append("success_action_status", '200');
+          formData.append("signature", policyData.signature);
+          formData.append("file", item.raw);
+          this.$ajax.post(
+            policyData.host,
+            formData
+          )
+            .then(
+              result => {
+                console.log(result.status)
+              }
+            )
+            .catch(
+              function (result) {
+                console.log(result)
+              }
+            )
+        },
+        getPolicy(item) {
+          if (this.getSuffix(item) == '.jpg' || this.getSuffix(item) == '.png') {
+            return this.$ajax.get('http://host.tanhuiri.cn:19527/policy/image');
+          } else {
+            return this.$ajax.get('http://host.tanhuiri.cn:19527/policy/file');
+          }
+        },
         submitUpload() {
-          this.$refs.upload.submit();
+          for (let item of this.fileList) {
+            // this.$refs.upload.submit();
+            this.getPolicy(item).then(
+              (res) => {
+                this.uploadFile(item, res.data.data);
+              }
+            ).then((res) => {
+              this.fileList.splice(0, this.fileList.length);
+            })
+              .catch(
+                function (result) {
+                  console.log(result)
+                }
+              )
+          }
+
+        },
+        // 用户选择文件的的闭包
+        handleChange(file, fileList) {
+          console.log(file)
+          console.log(fileList)
+          this.fileList.push(file)
         },
         handleRemove(file, fileList) {
+          this.fileList.splice(this.fileList.indexOf(file), 1)
           console.log(file, fileList);
+        },
+        beforeRemove(file, fileList) {
+          return this.$confirm(`确定移除 ${file.name}？`);
         },
         handlePreview(file) {
           console.log(file);
         },
-        handleSuccess(res, file) {
-          this.url = URL.createObjectURL(file.raw);
-        },
-        beforeUpload(file){
-          const isJPG = file.type === 'image/jpg';
-          const isPNG = file.type === 'image/png';
-          const isPDF = file.type === 'text/pdf'
+        beforeUpload(file) {
+          var suffix = this.getSuffix(file)
+          const isJPG = suffix === 'jpg'
+          const isPNG = suffix === 'png'
+          const isPDF = suffix === 'pdf'
           const isLt10M = file.size / 1024 / 1024 < 10;
-
-          if (!isJPG & !isPNG && !isPDF) {
-            this.$message.error('请上传正确格式的文件');
+          if (!isJPG && !isPNG && !isPDF) {
+            this.$message.error('上传文件只能是jpg，png或pdf格式!');
           }
           if (!isLt10M) {
             this.$message.error('上传文件大小不能超过10MB!');
           }
-          return (isJPG || isPDF || isPNG) && isLt10M;
+          return isLt10M;
+        },
+        getSuffix(file) {
+          return '.' + file.name.substring(file.name.lastIndexOf('.') + 1)
         }
+
       },
 
       data(){
@@ -91,18 +150,18 @@
 <style scoped>
   #specificAccess{
     width: 100%;
-    height: 100%;
+    height: auto;
   }
   .block{
     width: 1488px;
-    height: 840px;
+    height: auto;
     background-color: #FFFFFF;
     margin-left: 394px;
     margin-top: 28px;
   }
   .annotation {
     width: 1282px;
-    height: 149px;
+    height: 120px;
     background: #FFFFFF;
     margin-top: 30px;
     margin-left: 103px;
